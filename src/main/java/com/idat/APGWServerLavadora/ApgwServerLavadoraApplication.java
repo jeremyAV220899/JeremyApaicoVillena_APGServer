440d9e6d5909e21dccc5ec40b2f63c4a62d20062
jeremyav220899 <jeremy232129@gmail.com>
2022-06-03 21:53:15 -0500

    APGWserver-apaico

---
+++ src/main/java/com/idat/APGWServerLavadora/ApgwServerLavadoraApplication.java
@@ -0,0 +1,13 @@
+package com.idat.APGWServerLavadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApgwServerLavadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApgwServerLavadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/idat/APGWServerLavadora/ApgwServerLavadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.APGWServerLavadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApgwServerLavadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
